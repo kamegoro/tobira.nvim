1e0d0bdf344f3fc604cceabb74654f841039e6ee
kamegoro <tentyo0620@icloud.com>
2026-06-27 02:30:26 +0900

    style: fix stylua formatting in plugin/tobira.lua

--- plugin/tobira.lua
+++ plugin/tobira.lua
@@ -1,14 +1,16 @@
-if vim.g.loaded_tobira then return end
+if vim.g.loaded_tobira then
+  return
+end
 vim.g.loaded_tobira = true
 
-vim.api.nvim_create_user_command("Tobira", function()
-  require("tobira.core.suggest").manual()
-end, { desc = "Show next command suggestion" })
+vim.api.nvim_create_user_command('Tobira', function()
+  require('tobira.core.suggest').manual()
+end, { desc = 'Show next command suggestion' })
 
-vim.api.nvim_create_user_command("TobiraStats", function()
-  require("tobira.core.logger").stats()
-end, { desc = "Show command usage stats" })
+vim.api.nvim_create_user_command('TobiraStats', function()
+  require('tobira.core.logger').stats()
+end, { desc = 'Show command usage stats' })
 
-vim.api.nvim_create_user_command("TobiraReset", function()
-  require("tobira.core.logger").reset()
-end, { desc = "Reset usage log" })
+vim.api.nvim_create_user_command('TobiraReset', function()
+  require('tobira.core.logger').reset()
+end, { desc = 'Reset usage log' })
