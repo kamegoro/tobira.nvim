5fe2450a3ebcf7c577f927bc0833c122e6aa8b29
kamegoro <tentyo0620@icloud.com>
2026-06-27 11:03:23 +0900

    feat: add demo GIF
    Shows tobira detecting f_repeat pattern and displaying suggestion float.
    VHS tape + demo-init.lua + make-demo.sh allow local re-recording.
    
    Closes #11

--- docs/demo-init.lua
+++ docs/demo-init.lua
@@ -2,6 +2,7 @@
 -- Usage: nvim -u docs/demo-init.lua docs/demo.lua
 
 vim.opt.termguicolors = true
+vim.opt.swapfile = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
@@ -11,10 +12,19 @@ vim.opt.ruler = false
 vim.opt.showcmd = false
 vim.opt.showmode = false
 
--- Load tobira from this repo
+-- Load tobira from this repo root
 vim.opt.rtp:prepend(vim.fn.fnamemodify(debug.getinfo(1, 'S').source:sub(2), ':h:h'))
 
 require('tobira').setup({
-  idle_delay = 800, -- faster for demo
+  idle_delay = 800,
   max_shown = 3,
 })
+
+-- Pre-seed usage so :Tobira has something to show.
+-- Simulates a user who has been pressing 'f' but not ';'.
+vim.defer_fn(function()
+  local logger = require('tobira.core.logger')
+  for _ = 1, 10 do
+    logger.simulate_keys({ 'f', 'o' })
+  end
+end, 200)
